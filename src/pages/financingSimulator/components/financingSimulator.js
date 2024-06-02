b8161e3a2908c22503d316f703d27fbe057dbb44
joao-henrique10 <jhcalzavara@hotmail.com>
2024-06-02 10:20:18 -0300

    Add endpoint on financingSimulator to see results
    Co-authored-by: EuricoAbreu <euricoabreuneto@hotmail.com>

--- src/pages/financingSimulator/components/financingSimulator.js
+++ src/pages/financingSimulator/components/financingSimulator.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import ReactTooltip from 'react-tooltip';
-import { authAxiosInstance } from '../../../utils/AxiosConfig';
+import { authAxiosInstance as axios } from '../../../utils/AxiosConfig';
 
 const FinancingSimulator = () => {
   const [financingValue, setFinancingValue] = useState('');
@@ -8,6 +8,8 @@ const FinancingSimulator = () => {
   const [interestRate, setInterestRate] = useState('3.90');
   const [interestPeriod, setInterestPeriod] = useState('ao mês');
   const [amortizationType, setAmortizationType] = useState('Tabela Price');
+  const [simulationDetails, setSimulationDetails] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,24 +17,58 @@ const FinancingSimulator = () => {
   };
 
   const financingSimulation = async () => {
+    setLoading(true);
     try {
+      let calculation_method = '';
+      if (amortizationType.toLowerCase().includes('price')) {
+        calculation_method = 'price';
+      } else if (amortizationType.toLowerCase().includes('sac')) {
+        calculation_method = 'sac';
+      } else if (amortizationType.toLowerCase().includes('sacre')) {
+        calculation_method = 'sacre';
+      }
+
       const data = {
         amount: financingValue,
         duration_months: installments,
-        tax: interestRate
+        tax: interestRate,
+        calculation_method,
       };
 
-      console.log('Response', data);
-
-      const response = await authAxiosInstance.post(
+      const response = await axios.post(
         '/simulations/financing-simulation/', data
       );
-      console.log('Response', response.data);
-      
+
+      setSimulationDetails(response.data);
     } catch (error) {
       console.error('Erro ao fazer simulação:', error);
+    } finally {
+      setLoading(false);
     }
   };
+
+  const calculateTotals = (details) => {
+    let totalParcela = 0;
+    let totalAmortizacao = 0;
+    let totalJuros = 0;
+    let totalSaldoDevedor = 0;
+
+    details.forEach(detail => {
+      totalParcela += detail.parcela;
+      totalAmortizacao += detail.amortizacao;
+      totalJuros += detail.juros;
+      totalSaldoDevedor += detail.saldo_devedor;
+    });
+
+    return {
+      totalParcela,
+      totalAmortizacao,
+      totalJuros,
+      totalSaldoDevedor,
+    };
+  };
+
+  const formatNumber = (number) => Number(number).toFixed(2);
 
   return (
     <div className="bg-white p-6 rounded-lg shadow-md">
@@ -94,10 +130,50 @@ const FinancingSimulator = () => {
         <button
           type="submit"
           className="w-full bg-green-500 text-white p-2 rounded-md hover:bg-green-600"
+          disabled={loading}
         >
-          Simular Financiamento
+          {loading ? 'Simulando...' : 'Simular Financiamento'}
         </button>
       </form>
+      {simulationDetails && (
+        <div className="mt-6">
+          <h3 className="text-xl font-bold mb-4">Banco Recomendado</h3>
+          <p className="text-lg mb-2">Nome: {simulationDetails.bank_name}</p>
+          <p className="text-lg mb-4">Localização: {simulationDetails.bank_location}</p>
+          <h3 className="text-xl font-bold mb-4">Detalhes da Simulação</h3>
+          <div className="overflow-x-auto">
+            <table className="min-w-full bg-white border border-gray-300 text-center">
+              <thead>
+                <tr>
+                  <th className="py-2 px-4 border-b">#</th>
+                  <th className="py-2 px-4 border-b">Parcelas</th>
+                  <th className="py-2 px-4 border-b">Amortizações</th>
+                  <th className="py-2 px-4 border-b">Juros</th>
+                  <th className="py-2 px-4 border-b">Saldo Devedor</th>
+                </tr>
+              </thead>
+              <tbody>
+                {simulationDetails.details.map((detail, index) => (
+                  <tr key={index}>
+                    <td className="py-2 px-4 border-b">{index + 1}</td>
+                    <td className="py-2 px-4 border-b">{formatNumber(detail.parcela)}</td>
+                    <td className="py-2 px-4 border-b">{formatNumber(detail.amortizacao)}</td>
+                    <td className="py-2 px-4 border-b">{formatNumber(detail.juros)}</td>
+                    <td className="py-2 px-4 border-b">{formatNumber(detail.saldo_devedor)}</td>
+                  </tr>
+                ))}
+                <tr className="font-bold">
+                  <td className="py-2 px-4 border-b">»</td>
+                  <td className="py-2 px-4 border-b">{formatNumber(calculateTotals(simulationDetails.details).totalParcela)}</td>
+                  <td className="py-2 px-4 border-b">{formatNumber(calculateTotals(simulationDetails.details).totalAmortizacao)}</td>
+                  <td className="py-2 px-4 border-b">{formatNumber(calculateTotals(simulationDetails.details).totalJuros)}</td>
+                  <td className="py-2 px-4 border-b">« TOTAIS</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
